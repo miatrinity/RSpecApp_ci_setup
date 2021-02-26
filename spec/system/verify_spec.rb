ccc059241897efb4822f22b0157193cc1e2ab212
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2021-02-26 15:35:21 +0100

    Fix copy/paste lulz

--- spec/system/verify_spec.rb
+++ spec/system/verify_spec.rb
@@ -1,5 +1,9 @@
-RSpec.describe 'Visit verify page' do
-  visit verify_index_path
+require 'rails_helper' 
 
-  expect(page).to have_text('Hello, RSpec!')
+RSpec.describe 'Visit verify page' do
+  it "works" do
+    visit verify_index_path
+
+    expect(page).to have_text('Hello, RSpec!')
+  end
 end
